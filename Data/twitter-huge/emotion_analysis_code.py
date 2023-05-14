f696027d8f21bd27c64102261d37a5b511945ccb
Xinyi323 <132901573+Xinyi323@users.noreply.github.com>
2023-05-14 20:18:03 +1000

    Update emotion_analysis_code.py

--- Data/twitter-huge/emotion_analysis_code.py
+++ Data/twitter-huge/emotion_analysis_code.py
@@ -1,29 +1,51 @@
 #### emotion analysis -- pos/neg/neu
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+#### extract tweets about marriage etc.
 import json
 import ijson
+import gensim.downloader as api
 import nltk
 from nltk.corpus import wordnet
-from nltk.sentiment import SentimentIntensityAnalyzer
 
+# Load pre-trained Word2Vec model
+w2v_model = api.load('fasttext-wiki-news-subwords-300')
 
-def get_synonyms(words):
+# Define keywords
+keywords = ["married", "defacto", "divorced", "separated", "widowed", "unmarried"]
+
+# Get similar words to each keyword and concatenate them
+similar_words = []
+for keyword in keywords:
+    similar_words += [word[0] for word in w2v_model.most_similar(keyword)]
+
+# Remove duplicates and print the resulting list
+similar_words = list(set(similar_words))
+
+def get_synonyms(words, languages):
     synonyms = []
     for word in words:
-        for syn in wordnet.synsets(word):
-            for lemma in syn.lemmas():
-                synonyms.append(lemma.name())
+        for language in languages:
+            synsets = wordnet.synsets(word, lang=language)
+            for syn in synsets:
+                for lemma in syn.lemmas(lang=language):
+                    synonyms.append(lemma.name())
     return synonyms
 
+keywords = ["married", "de facto", "divorced", "separated", "widowed", "never married"]
+languages = nltk.corpus.wordnet.langs()
+synonyms = get_synonyms(similar_words, languages)
+
+# Remove duplicates and print the resulting list
+synonyms = list(set(synonyms))
+synonyms += list(set(similar_words) - set(synonyms))
+print(synonyms)
 
 def analyze_sentiment(text):
     sia = SentimentIntensityAnalyzer()
     sentiment_scores = sia.polarity_scores(text)
     compound_score = sentiment_scores['compound']
     return compound_score
-
-
-keywords = ["married", "de facto", "divorced", "separated", "widowed", "never married"]
-synonyms = get_synonyms(keywords)
 
 marital_data = []
 results = []
@@ -33,9 +55,7 @@ positive_count = 0
 negative_count = 0
 neutral_count = 0
 
-file_path = "E:\\Desktop\\data\\twitter-huge.json\\mnt\\ext100\\twitter-huge.json"
-
-with open(file_path, 'r', encoding='utf-8') as f:
+with open('twitter-huge.json', 'r', encoding='utf-8') as f:
     parser = ijson.parse(f)
     is_matching_tweet = False
     for prefix, event, value in parser:
@@ -70,6 +90,8 @@ with open(file_path, 'r', encoding='utf-8') as f:
                     }
                     print(tweet_data)
                     results.append(tweet_data)
+                    if count>100:
+                        break;
             else:
                 is_matching_tweet = False
         elif is_matching_tweet and prefix.endswith('.tweet.id_str') and event == 'string':
