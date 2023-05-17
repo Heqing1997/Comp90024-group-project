17e48c382ecf2f93844d367375a3d45bd689480b
Xinyi323 <132901573+Xinyi323@users.noreply.github.com>
2023-05-17 15:25:05 +1000

    Update emotion_to_couchdb.py

--- Data/twitter-huge/emotion_to_couchdb.py
+++ Data/twitter-huge/emotion_to_couchdb.py
@@ -12,7 +12,7 @@ server_url = f'http://{admin}:{password}@172.26.135.144:5984/'
 couch = couchdb.Server(server_url)
 
 # indicate the db name
-db_name = 'emotion_analysis'
+db_name = 'twitters_marriage_informations'
 
 # if not exist, create one
 if db_name not in couch:
@@ -20,21 +20,11 @@ if db_name not in couch:
 else:
     db = couch[db_name]
 
-# Specify the path and filename of the JSON file
-json_file_path = 'E:\Desktop\emotion_analysis.json'
-
 # Open the JSON file and load the data
-with open(json_file_path, 'r') as json_file:
+with open('emotion_analysis.json', 'r') as json_file:
     content = json.load(json_file)
 
 # Import the JSON data to CouchDB
 for index, data in enumerate(content):
     doc_id, doc_rev = db.save(data)
     print(f'Document {index + 1} saved with ID: {doc_id} and revision: {doc_rev}')
-
-# basic query all the docs
-for _id in db:
-    doc = db.get(_id)
-    # filter and do something
-    print(f"ID: {_id}, Rev: {doc['_rev']}")
-
